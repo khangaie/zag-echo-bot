78027ee2803c52f0da4666ca3dc9343a0f870474
khangaie <khangai.e@zag.mn>
2026-01-28 16:50:00 +0800

    Update sharepoint.js

--- graph/sharepoint.js
+++ graph/sharepoint.js
@@ -1,14 +1,38 @@
 const axios = require('axios');
 
 /**
+ * Microsoft Graph access token авах (Client Credentials)
+ */
+async function getGraphToken() {
+  const tenantId = process.env.MicrosoftAppTenantId;
+  const clientId = process.env.MicrosoftAppId;
+  const clientSecret = process.env.MicrosoftAppPassword;
+
+  if (!tenantId || !clientId || !clientSecret) {
+    throw new Error('Azure AD app environment variables дутуу байна');
+  }
+
+  const tokenUrl = `https://login.microsoftonline.com/${tenantId}/oauth2/v2.0/token`;
+
+  const params = new URLSearchParams();
+  params.append('client_id', clientId);
+  params.append('client_secret', clientSecret);
+  params.append('scope', 'https://graph.microsoft.com/.default');
+  params.append('grant_type', 'client_credentials');
+
+  const response = await axios.post(tokenUrl, params, {
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+  });
+
+  return response.data.access_token;
+}
+
+/**
  * SharePoint дээр текст хайх
- * @param {string} accessToken - Microsoft Graph access token
  * @param {string} query - Хэрэглэгчийн асуулт
  */
-async function searchSharePoint(accessToken, query) {
-  if (!accessToken) {
-    throw new Error('GRAPH_TOKEN байхгүй байна');
-  }
+async function searchSharePoint(query) {
+  const accessToken = await getGraphToken();
 
   const url = 'https://graph.microsoft.com/v1.0/search/query';
 

4a93f60f05453caedd3a6d121018beab92fdcbb7
khangaie <khangai.e@zag.mn>
2026-01-30 10:25:18 +0800

    Update sharepointSearch.js

--- graph/sharepointSearch.js
+++ graph/sharepointSearch.js
@@ -1,46 +1,21 @@
 const axios = require('axios');
 
 async function searchSharePoint(query, accessToken) {
-  const url = 'https://graph.microsoft.com/v1.0/search/query';
+  const siteId = process.env.SHAREPOINT_SITE_ID;
 
-  const payload = {
-    requests: [
-      {
-        entityTypes: ['DriveItem'], // ⬅️ TOM D ҮСЭГТЭЙ!
-        query: {
-          queryString: query
-        },
-        from: 0,
-        size: 5
-      }
-    ]
-  };
+  const url = `https://graph.microsoft.com/v1.0/sites/${siteId}/drive/root/search(q='${encodeURIComponent(query)}')`;
 
-  try {
-    const res = await axios.post(url, payload, {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-        'Content-Type': 'application/json'
-      }
-    });
+  const res = await axios.get(url, {
+    headers: {
+      Authorization: `Bearer ${accessToken}`
+    }
+  });
 
-    const hits =
-      res.data.value?.[0]?.hitsContainers?.[0]?.hits || [];
-
-    return hits.map(h => ({
-      fileName: h.resource?.name,
-      url: h.resource?.webUrl,
-      folder: h.resource?.parentReference?.path || '',
-      content: h.resource?.summary || ''
-    }));
-
-  } catch (err) {
-    console.error(
-      'SharePoint search error:',
-      err.response?.data || err.message
-    );
-    throw err;
-  }
+  return res.data.value.map(item => ({
+    fileName: item.name,
+    url: item.webUrl,
+    folder: item.parentReference?.path || '',
+  }));
 }
 
 module.exports = { searchSharePoint };

b7e2e47ba3cbca8804431dfb9dd4ddf52cac840a
khangaie <khangai.e@zag.mn>
2026-01-31 16:57:34 +0800

    Update sharepointSearch.js

--- graph/sharepointSearch.js
+++ graph/sharepointSearch.js
@@ -1,31 +1,37 @@
-const axios = require("axios");
-const { getGraphToken } = require("./token");
-async function searchSharePointFiles(query) {
- const accessToken = await getGraphToken();
- const url = `https://graph.microsoft.com/v1.0/search/query`;
+const axios = require('axios');
+/**
+* SharePoint дээр файл хайх
+*/
+async function searchSharePoint({ query, accessToken }) {
+ if (!accessToken) {
+   throw new Error('accessToken байхгүй');
+ }
+ const endpoint = 'https://graph.microsoft.com/v1.0/search/query';
  const body = {
    requests: [
      {
-       entityTypes: ["driveItem"],
+       entityTypes: ['driveItem'],
        query: {
-         queryString: query,
+         queryString: query
        },
        from: 0,
-       size: 5,
-     },
-   ],
+       size: 5
+     }
+   ]
  };
- const res = await axios.post(url, body, {
+ const res = await axios.post(endpoint, body, {
    headers: {
      Authorization: `Bearer ${accessToken}`,
-     "Content-Type": "application/json",
-   },
+     'Content-Type': 'application/json'
+   }
  });
- // driveItem → хэрэгтэй хэлбэрт оруулах
- return res.data.value[0].hitsContainers[0].hits.map((h) => ({
+ const hits =
+   res.data?.value?.[0]?.hitsContainers?.[0]?.hits || [];
+ return hits.map(h => ({
    name: h.resource.name,
-   downloadUrl: h.resource["@microsoft.graph.downloadUrl"],
-   webUrl: h.resource.webUrl,
+   webUrl: h.resource.webUrl
  }));
 }
-module.exports = { searchSharePointFiles };
+module.exports = {
+ searchSharePoint
+};

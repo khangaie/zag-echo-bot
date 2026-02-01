846abc608f6bc14811207430c145ec22fa5ca95a
khangaie <khangai.e@zag.mn>
2026-02-01 20:46:38 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-const express = require('express');
-const { BotFrameworkAdapter } = require('botbuilder');
-const ZAGBot = require('./bot');
+const express = require("express");
+const { BotFrameworkAdapter } = require("botbuilder");
+const ZAGBot = require("./bot");
 const adapter = new BotFrameworkAdapter({
  appId: process.env.MicrosoftAppId,
  appPassword: process.env.MicrosoftAppPassword
 });
 adapter.onTurnError = async (context, error) => {
- console.error(error);
- await context.sendActivity('⚠️ Системийн алдаа гарлаа.');
+ console.error("ADAPTER ERROR:", error);
+ await context.sendActivity("⚠️ Системийн алдаа гарлаа.");
 };
 const bot = new ZAGBot();
 const app = express();
 app.use(express.json());
-app.get('/', (_, res) => {
- res.send('✅ ZAG Teams Bot is running');
+app.get("/", (_, res) => {
+ res.send("✅ ZAG Teams Bot is running");
 });
-app.post('/api/messages', (req, res) => {
+app.post("/api/messages", (req, res) => {
  adapter.processActivity(req, res, (context) => bot.run(context));
 });
 const port = process.env.PORT || 8080;

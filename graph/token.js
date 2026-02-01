bf95c49e7ed6c618299002be9c49cdb7013b7ba9
khangaie <khangai.e@zag.mn>
2026-02-01 20:32:27 +0800

    Update token.js

--- graph/token.js
+++ graph/token.js
@@ -1,18 +1,15 @@
-const { ConfidentialClientApplication } = require('@azure/msal-node');
-
+const { ConfidentialClientApplication } = require("@azure/msal-node");
 const msalClient = new ConfidentialClientApplication({
-  auth: {
-    clientId: process.env.MicrosoftAppId,
-    authority: `https://login.microsoftonline.com/${process.env.MicrosoftAppTenantId}`,
-    clientSecret: process.env.MicrosoftAppPassword
-  }
+ auth: {
+   clientId: process.env.MicrosoftAppId,
+   authority: `https://login.microsoftonline.com/${process.env.MicrosoftAppTenantId}`,
+   clientSecret: process.env.MicrosoftAppPassword
+ }
 });
-
 async function getGraphToken() {
-  const result = await msalClient.acquireTokenByClientCredential({
-    scopes: ['https://graph.microsoft.com/.default']
-  });
-  return result.accessToken;
+ const result = await msalClient.acquireTokenByClientCredential({
+   scopes: ["https://graph.microsoft.com/.default"]
+ });
+ return result.accessToken;
 }
-
 module.exports = { getGraphToken };

eb2d39d0a6500d9c1e13c5222d745f745514d742
khangaie <khangai.e@zag.mn>
2026-02-04 00:33:16 +0800

    Update sharepointSearch.js

--- graph/sharepointSearch.js
+++ graph/sharepointSearch.js
@@ -1,29 +1,19 @@
 const axios = require("axios");
+
 async function searchSharePoint(query, accessToken) {
- const url = "https://graph.microsoft.com/v1.0/search/query";
- const payload = {
-   requests: [
-     {
-       entityTypes: ["driveItem"],
-       query: {
-         queryString: query
-       },
-       from: 0,
-       size: 5
-     }
-   ]
- };
- const res = await axios.post(url, payload, {
-   headers: {
-     Authorization: `Bearer ${accessToken}`,
-     "Content-Type": "application/json"
-   }
- });
- const hits =
-   res.data?.value?.[0]?.hitsContainers?.[0]?.hits || [];
- return hits.map(h => ({
-   name: h.resource?.name,
-   url: h.resource?.webUrl
- }));
+  const url = "https://graph.microsoft.com/v1.0/search/query";
+  const payload = {
+    requests: [
+      { entityTypes: ["driveItem"], query: { queryString: query }, from: 0, size: 5 }
+    ]
+  };
+
+  const res = await axios.post(url, payload, {
+    headers: { Authorization: `Bearer ${accessToken}`, "Content-Type": "application/json" }
+  });
+
+  const hits = res.data?.value?.[0]?.hitsContainers?.[0]?.hits || [];
+  return hits.map(h => ({ name: h.resource?.name, url: h.resource?.webUrl }));
 }
+
 module.exports = { searchSharePoint };
